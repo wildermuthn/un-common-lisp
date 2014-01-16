620692000b83856a726b88a3e6a05ce02fc8bed1
root <root@un-common-lisp.(none)>
2014-01-16 13:42:16 -0500

    Added serialization to disk

--- blog.lisp
+++ blog.lisp
@@ -1,4 +1,4 @@
-;;; Blog Package for un-common-lisp.com
+;;; blog package for un-common-lisp.com
 
 (in-package :com.un-common-lisp.blog)
 
@@ -11,8 +11,19 @@
 ;; Save and Restore and Flush Database
 (defun save-db ()
   (progn
-  (setf *saved-posts* *posts*)))
+    (setf *saved-posts* *posts*)
+    (with-open-file (stream "/srv/blog-db/posts.db" :direction :output :if-exists :supersede)
+      (print (ms:marshal *posts*) stream))) )
 
+(defun load-db ()
+  (with-open-file (s "/srv/blog-db/posts.db")
+    (setf *posts* (loop with eof = (list nil)
+                        for form = (read s nil eof)
+                        until (eq form eof)
+                        collect form into forms
+                        counting t into form-count
+                        finally (return (apply #'ms:unmarshal (values forms form-count)))))))
+    
 (defun restore-db ()
   (setf *posts* *saved-posts*))
 
@@ -44,21 +55,25 @@
    (summary :initarg :summary :accessor summary :initform (random-word-s 25))
    (text :initarg :text :accessor text :initform (random-word-s 200))))
 
+(defmethod ms:class-persistant-slots ((self post))
+  '(title post-id post-time author summary text))
+
 (defmethod print-object ((object post) stream)
     (print-unreadable-object (object stream :type t)
           (with-slots (title post-id) object
                   (format stream "~%title: ~a~%post-id:~a" title post-id))))
 
 (defun create-post (&key title summary text author)
-  (push (make-instance 'post :title title :summary summary :text text :author author) *posts*) )
+  (progn
+    (push (make-instance 'post :title title :summary summary :text text :author author) *posts*)
+    (save-db)))
   
 (defun create-random-post ()
   (push (make-instance 'post) *posts*))
   
 (defun populate-db (length)
   (progn
-    (dotimes (n length n) (create-random-post))
-    (make-first-post)))
+    (dotimes (n length n) (create-random-post))))
   
 (defun set-post-timestamp (id timestamp)
   (dolist (lst *posts* lst)
@@ -77,7 +92,6 @@
 (defun two-words () 
   (concatenate 'string (random-word (max 3 (random 8))) " " (random-word (max 3 (random 8)))))
 
-;; Hunchentoot Definitions 
  
 (hunchentoot:define-easy-handler (some-handler-post :uri "/rest/all-posts") ()
   (setf (hunchentoot:content-type*) "application/json")
